99ae6fa8ed449c372892822650038479ca0ecdcd
jpwol <jpwolfe1981@gmail.com>
2025-03-05 22:49:59 -0500

    added function declarations

--- include/SharpSurface.h
+++ include/SharpSurface.h
@@ -6,6 +6,7 @@
 #include <immintrin.h>
 #include <stdlib.h>
 
+#include "SharpPoint.h"
 #include "SharpRect.h"
 #include "SharpWindow.h"
 
@@ -21,14 +22,22 @@ typedef struct SharpSurface {
 } SharpSurface;
 
 SharpSurface* SharpGetWindowSurface(SharpWindow* window);
-void SharpSetDrawColor(SharpSurface* surface, int r, int g, int b);
+SharpSurface* SharpCreateSurface(SharpWindow* window, int width, int height);
+
 void SharpClearSurfaceColor(SharpSurface* surface, unsigned int color);
+
 void SharpDrawRect(SharpSurface* surface, SharpRect* rect);
+
 void SharpFillRect(SharpSurface* surface, SharpRect* rect, unsigned int color);
-void SharpFillColor(SharpSurface* surface);
-void SharpDestroySurface(SharpSurface* surface);
+
+void SharpDrawPoint(SharpSurface* surface, SharpPoint* point,
+                    unsigned int color);
+
 void SharpUpdateWindowSurface(SharpWindow* window);
+
 void SharpBlitSurface(SharpSurface* surface);
+
+void SharpDestroySurface(SharpSurface* surface);
 
 #ifdef __cplusplus
 }
